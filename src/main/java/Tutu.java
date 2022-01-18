03612e2d68a1e5b9f1f9da664f2ce730b96a78ea
chinkiat <e0544173@u.nus.edu>
2022-01-18 21:17:48 +0800

    added add and list functionality

--- src/main/java/Tutu.java
+++ src/main/java/Tutu.java
@@ -1,8 +1,20 @@
 import java.util.*;
 
 public class Tutu {
+    private static ArrayList<String> items = new ArrayList<>();
     public static void separator() {
         System.out.println("******************************************");
+    }
+    public static void add(String cmd) {
+        items.add(cmd);
+        System.out.println("added: " + cmd);
+        Tutu.separator();
+    }
+    public static void list() {
+        for (int i = 1; i <= items.size(); i++) {
+            System.out.println(i + ". " + items.get(i - 1));
+        }
+        Tutu.separator();
     }
     public static void main(String[] args) {
         Tutu.separator();
@@ -11,8 +23,11 @@ public class Tutu {
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
         while (!cmd.equals("bye")) {
-            System.out.println(cmd);
-            Tutu.separator();
+            if (cmd.equals("list")) {
+                list();
+            } else {
+                add(cmd);
+            }
             cmd = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
