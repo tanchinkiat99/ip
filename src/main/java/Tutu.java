6fc04b9c82e7e5abfacccf5ff40b90176f0baeb2
chinkiat <e0544173@u.nus.edu>
2022-01-18 17:34:52 +0800

    customized display format

--- src/main/java/Tutu.java
+++ src/main/java/Tutu.java
@@ -1,12 +1,18 @@
 import java.util.*;
 
 public class Tutu {
+    public static void separator() {
+        System.out.println("******************************************");
+    }
     public static void main(String[] args) {
+        Tutu.separator();
         System.out.println("Hello! I'm Tutu\nWhat can I do for you?");
+        Tutu.separator();
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
         while (!cmd.equals("bye")) {
             System.out.println(cmd);
+            Tutu.separator();
             cmd = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
